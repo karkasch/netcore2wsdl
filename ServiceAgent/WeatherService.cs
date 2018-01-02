730c61ab9c0508df2c8f3b2347e63d1cecc0881b
karkasch <nutzer12@gmail.com>
2018-01-02 20:00:59 +0100

    init

---
+++ ServiceAgent/WeatherService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using GlobalWeather;
+
+
+namespace ServiceAgent
+{
+    public class WeatherService
+    {
+
+        public async Task<string> Test()
+        {
+            var addr = "http://www.webservicex.com/globalweather.asmx";
+            var client = new GlobalWeatherSoapClient(GlobalWeatherSoapClient.EndpointConfiguration.GlobalWeatherSoap, addr);
+
+            //var res = await client.GetWeatherAsync("utrecht", "netherlands");
+
+            var cities = await client.GetCitiesByCountryAsync("netherlands");
+
+            await client.CloseAsync();
+
+            return cities;
+        }
+    }
+}
